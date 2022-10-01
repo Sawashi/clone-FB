ce75efb9f36c07323e7d135d8ae1da52b292a6fa
Sawashi <kietle234.cc@gmail.com>
2022-10-01 17:25:19 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./index.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Homepage from "./pages/homepage/Homepage";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="cloneFB/home" element={<Homepage />} default />
+        <Route path="*" element={<Navigate to="cloneFB/home" replace />} />
+      </Routes>
+    </>
   );
 }
 
